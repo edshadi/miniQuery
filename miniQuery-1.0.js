d4eb5d6ad2e1b27bbe3c1924148e2ca7f7bf7358
Ed Shadi <edshadi@gmail.com>
2014-02-15 11:56:02 -0800

    miniQuery 1.0: show, hide, addClass and removeClass support

---
+++ miniQuery-1.0.js
@@ -0,0 +1,57 @@
+/*!
+ * minQuery v1.0
+ *
+ * Released under the SHADI license
+ *
+ * show, hide, addClass and removeClass from DOM elements
+ * usage: same as jQuery ;)
+ */
+var miniQuery = (function(pubsub) {
+  var _$ = function(selector) {
+    var type = selector.charAt(0),
+        selector = selector.match(/^\W/) ? selector.slice(1) : selector,
+        element;
+    switch(type) {
+     case '.':
+       element = document.getElementsByClassName(selector)[0];
+       break;
+     case '#':
+       element = document.getElementById(selector);
+       break;
+     default:
+       element = document.getElementsByTagName(selector);
+       break;
+    }
+   if(element) return new MiniQuery(element);
+  };
+
+  var MiniQuery = function(element) {
+    this.element = element;
+  }
+  MiniQuery.prototype = {
+    show: function() {
+      this.element.setAttribute('style', 'display: block;');
+    },
+    hide: function() {
+      this.element.setAttribute('style', 'display: none;');
+    },
+    addClass: function(name) {
+      var klass = this.element.className + ' ' + name;
+      this.element.setAttribute('class', klass.trim());
+    },
+    removeClass: function(name) {
+      var klass = this.element.className.replace(name, '').trim();
+      this.element.setAttribute('class', klass);
+    }
+  }
+  return function(global) {
+    global.$ = _$;
+  }
+}());
+
+
+(function(global, miniQuery) {
+  if (!global) throw new Error( "we need a window!" );
+  miniQuery(global);
+}(window, miniQuery));
+
